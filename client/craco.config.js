a90490f445a483ca84e6e731ba15b658f1453835
Krishna Sathvik <krishnasathvikmantripragada@gmail.com>
2025-10-07 15:19:59 -0500

    Standardize on React 18; CRACO build; modern schema-utils/ajv; Vercel Node 20

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,19 +1,31 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // Remove fork-ts-checker-webpack-plugin completely
-      webpackConfig.plugins = webpackConfig.plugins.filter(plugin => {
-        return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin';
-      });
-      
-      // Disable TypeScript checking entirely
-      webpackConfig.resolve = webpackConfig.resolve || {};
-      webpackConfig.resolve.alias = {
-        ...webpackConfig.resolve.alias,
-        // Disable TypeScript by aliasing it to a dummy module
-      };
-      
-      return webpackConfig;
-    },
-  },
+    configure: (config) => {
+      // Remove ForkTsChecker (which drags schema-utils v2 behavior)
+      config.plugins = config.plugins.filter(
+        (p) => !(p && p.constructor && p.constructor.name === 'ForkTsCheckerWebpackPlugin')
+      );
+
+      // Ensure modern terser
+      if (config.optimization && Array.isArray(config.optimization.minimizer)) {
+        config.optimization.minimizer = config.optimization.minimizer.map((m) => {
+          if (m && m.constructor && m.constructor.name === 'TerserPlugin') {
+            return new TerserPlugin({
+              parallel: true,
+              extractComments: false,
+              terserOptions: {
+                compress: { comparisons: false },
+                mangle: true,
+                format: { comments: false }
+              }
+            });
+          }
+          return m;
+        });
+      }
+      return config;
+    }
+  }
 };
