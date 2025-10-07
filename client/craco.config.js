adcaa29bce39b69919ae3d478e4af64d53850982
Krishna Sathvik <krishnasathvikmantripragada@gmail.com>
2025-10-06 20:07:37 -0500

    Simplify Craco config to avoid React Refresh conflicts

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,14 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  babel: {
-    plugins: [
-      // Remove console statements in production
-      ...(isProduction ? ['transform-remove-console'] : []),
-      // Disable React Refresh in production
-      ...(isProduction ? [] : ['react-refresh/babel']),
-    ],
-  },
   webpack: {
     configure: (webpackConfig) => {
       if (isProduction) {
