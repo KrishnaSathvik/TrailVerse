3e6850bf9da0c72df9b9657bebaabe0a8a0d1548
Krishna Sathvik <krishnasathvikmantripragada@gmail.com>
2025-10-06 20:06:43 -0500

    Fix React Refresh Babel transform error in production build

--- client/craco.config.js
+++ client/craco.config.js
@@ -5,11 +5,18 @@ module.exports = {
     plugins: [
       // Remove console statements in production
       ...(isProduction ? ['transform-remove-console'] : []),
+      // Disable React Refresh in production
+      ...(isProduction ? [] : ['react-refresh/babel']),
     ],
   },
   webpack: {
     configure: (webpackConfig) => {
       if (isProduction) {
+        // Disable React Refresh in production
+        webpackConfig.plugins = webpackConfig.plugins.filter(
+          plugin => plugin.constructor.name !== 'ReactRefreshPlugin'
+        );
+        
         // Additional webpack optimizations for production
         webpackConfig.optimization = {
           ...webpackConfig.optimization,
