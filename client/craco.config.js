b209cb270e334c7e85cfc1aae4eff12fa814962a
Krishna Sathvik <krishnasathvikmantripragada@gmail.com>
2025-10-06 22:39:37 -0500

    Override babel config in Craco to completely disable React Refresh

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,14 +1,37 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  babel: {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: false,
+          runtime: 'automatic',
+        },
+      ],
+    ],
+    plugins: [
+      // Remove console statements in production builds
+      ...(isProduction ? ['transform-remove-console'] : []),
+    ],
+  },
   webpack: {
     configure: (webpackConfig) => {
+      // Always disable React Refresh
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        plugin => plugin.constructor.name !== 'ReactRefreshPlugin'
+      );
+      
       if (isProduction) {
-        // Disable React Refresh in production
-        webpackConfig.plugins = webpackConfig.plugins.filter(
-          plugin => plugin.constructor.name !== 'ReactRefreshPlugin'
-        );
-        
         // Additional webpack optimizations for production
         webpackConfig.optimization = {
           ...webpackConfig.optimization,
