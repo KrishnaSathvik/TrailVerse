ce01b3bc43654683b0e7360901cc6e45eee1c821
Krishna Sathvik <krishnasathvikmantripragada@gmail.com>
2025-10-07 14:34:43 -0500

    Completely disable fork-ts-checker-webpack-plugin in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 module.exports = function override(config, env) {
   const isProduction = env === 'production';
   
+  // Disable fork-ts-checker-webpack-plugin to avoid ajv-keywords conflicts
+  if (config.plugins) {
+    config.plugins = config.plugins.filter(plugin => {
+      return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin';
+    });
+  }
+  
   if (isProduction) {
     // Add plugin to remove console statements in production
     config.optimization = {
